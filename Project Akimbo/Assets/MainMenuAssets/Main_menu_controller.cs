41a163aa7b9d708bd35ee4904c9ad2ed78a55c44
Daniel Sandoval <dansan3420@gmail.com>
2020-07-12 17:23:21 -0700

    Scraps of the main menu

---
+++ Project Akimbo/Assets/MainMenuAssets/Main_menu_controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu_controller : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("First_Scene");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Game has quit!");
+        Application.Quit();
+    }
+}
