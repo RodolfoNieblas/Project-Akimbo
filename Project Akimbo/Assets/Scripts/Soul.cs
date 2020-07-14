ef9066f907947e9e945e2747c7ec78607f5f98de
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-13 21:02:31 -0700

    Soul
    It can be any color and it follows you

---
+++ Project Akimbo/Assets/Scripts/Soul.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soul : MonoBehaviour //This script goes on the soul
+{
+    public float soulSpeed = 1f; //Just in case we forget to set it in game
+
+    private Transform playerTM;
+    private Rigidbody2D soulRB;
+    private float soulX;
+    private float soulY;
+    private float playerX;
+    private float playerY;
+    private float soulVelX;
+    private float soulVelY;
+
+    public float  leftShoulderX = 0.3f;
+    public float  rightShoulderX = 0.3f;
+    public float shoulderY = 0.7f;
+    private float leftShoulderXprivate;
+    private float rightShoulderXprivate;
+    private float shoulderYprivate;
+
+    void Start()
+    {
+        playerTM = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        soulRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+
+    void Update()
+    {
+        soulX = this.transform.position.x;
+        soulY = this.transform.position.y;
+
+        playerX = playerTM.position.x;
+        playerY = playerTM.position.y;
+
+        leftShoulderXprivate = (playerX - leftShoulderX) - soulX;
+        rightShoulderXprivate = (playerX + rightShoulderX) - soulX;
+        shoulderYprivate = (playerY + shoulderY) - soulY;
+
+        if((playerX - soulX) <= 0f) //if velocity vector is positive, go on left shoulder
+        {
+            soulRB.velocity = new Vector2(rightShoulderXprivate * soulSpeed, shoulderYprivate * soulSpeed);
+        } 
+        else if((playerX - soulX) > 0f) //if velocity vector is negative, go on right shoulder
+        {
+            soulRB.velocity = new Vector2(leftShoulderXprivate * soulSpeed, shoulderYprivate * soulSpeed);
+        }
+        
+    }
+}
