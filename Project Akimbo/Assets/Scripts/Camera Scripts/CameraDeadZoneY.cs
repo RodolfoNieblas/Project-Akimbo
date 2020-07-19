61f5c5373ee49a45ecacb18bb50b24c4755a7824
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-07-19 14:03:48 -0700

    yee

---
+++ Project Akimbo/Assets/Scripts/Camera Scripts/CameraDeadZoneY.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDeadZoneY : MonoBehaviour
+{
+    //need variable for boolean in camera follow
+    private CameraFollow camerafollow;
+
+    void Start(){
+        camerafollow = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            camerafollow.cameraFollowingX = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            camerafollow.cameraFollowingX = true;
+        }
+    }
+}
---
+++ Project Akimbo/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour //this goes on the main camera
+{
+    
+    public float dampTime = 0.4f; //for smoothness
+    public float zAxis = -20f;
+    public bool cameraFollowingX = true; //this is being manipulated on another script
+    public bool cameraFollowingY = true; //this is being manipulated on another script
+    private Transform Player;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero; 
+    
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+
+    }
+    
+    void Update()
+    {
+        
+
+        if(cameraFollowingX == true)
+        {
+            cameraPos = new Vector3(Player.position.x, Player.position.y, zAxis);
+        }
+        else if(cameraFollowingX == false)
+        {
+            cameraPos = new Vector3(transform.position.x, Player.position.y, zAxis);
+        }
+
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
